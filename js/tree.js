e150bcfdc0dc0d5e0a39f0a231caa934e30509c0
Zahra Shivji <zshivji@caltech.edu>
2026-02-03 19:18:17 -0800

    adding success/error message for metadata load

--- js/tree.js
+++ js/tree.js
@@ -1,46 +1,76 @@
 console.log("tree.js is running");
 
-fetch("./tree.svg")
-  .then(r => r.text())
-  .then(svg => {
-    document.getElementById("tree-container").innerHTML = svg;
-    attachTooltips();
-    console.log("SVG injected");
-  })
-  .catch(console.error);
-
-fetch("./metadata.json").then(r => r.json())
+Promise.all([
+  fetch("./tree.svg").then(r => r.text()),
+  fetch("./metadata.json").then(r => r.json())
+]).then(([svg, metadata]) => {
+  document.getElementById("tree-container").innerHTML = svg;
+  console.log("SVG + metadata loaded");
+  attachTooltips(metadata);
+}).catch(console.error);
 
 
-const tooltip = document.getElementById("tooltip");
+//   const metadata = metadata;
+//   console.log("metadata fetched");
+
+//   .then(svg => {
+//     document.getElementById("tree-container").innerHTML = svg;
+//     console.log("SVG injected");
+//   })
+//   .catch(console.error);
+
+// fetch("./metadata.json")
+//   .then(r => r.json())
+//   .then(data => {metadata = data; console.log("metadata fetched");})
+//   .catch(console.error);
 
 
-// iTOL leaf labels are <text> elements
-const labels = document.querySelectorAll("svg text");
 
-labels.forEach(label => {
-  const id = label.textContent.trim();
 
-  if (!metadata[id]) return;
 
-  label.classList.add("tree-label");
+// fetch("./tree.svg")
+//   .then(r => r.text())
+//   .then(svg => {
+//     document.getElementById("tree-container").innerHTML = svg;
+//     console.log("SVG injected");
+//   })
+//   .catch(console.error);
 
-  label.addEventListener("mouseover", e => {
-    tooltip.innerHTML = `
-      <b>${id}</b><br>
-      ${metadata[id].species}<br>
-      ${metadata[id].taxonomy}<br>
-      ${metadata[id].environment}
-    `;
-    tooltip.style.display = "block";
-  });
+// fetch("./metadata.json")
+//   .then(r => r.json())
+//   .then(data => {metadata = data; console.log("metadata fetched");})
+//   .catch(console.error);
 
-  label.addEventListener("mousemove", e => {
-    tooltip.style.left = e.pageX + 10 + "px";
-    tooltip.style.top = e.pageY + 10 + "px";
-  });
 
-  label.addEventListener("mouseout", () => {
-    tooltip.style.display = "none";
-  });
-});
+// const tooltip = document.getElementById("tooltip");
+
+
+// // iTOL leaf labels are <text> elements
+// const labels = document.querySelectorAll("svg text");
+
+// labels.forEach(label => {
+//   const id = label.textContent.trim();
+
+//   if (!metadata[id]) return;
+
+//   label.classList.add("tree-label");
+
+//   label.addEventListener("mouseover", e => {
+//     tooltip.innerHTML = `
+//       <b>${id}</b><br>
+//       ${metadata[id].species}<br>
+//       ${metadata[id].taxonomy}<br>
+//       ${metadata[id].environment}
+//     `;
+//     tooltip.style.display = "block";
+//   });
+
+//   label.addEventListener("mousemove", e => {
+//     tooltip.style.left = e.pageX + 10 + "px";
+//     tooltip.style.top = e.pageY + 10 + "px";
+//   });
+
+//   label.addEventListener("mouseout", () => {
+//     tooltip.style.display = "none";
+//   });
+// });
