e036d9c270180dc521fd190612dc8725876ef6ff
Zahra Shivji <zshivji@caltech.edu>
2026-02-05 12:11:42 -0800

    adding tooltip fxn

--- js/tree.js
+++ js/tree.js
@@ -1,52 +1,51 @@
 console.log("tree.js is running");
 
+
+// grab iTOL leaf labels (<text> elements)
 function attachTooltips(metadata) {
   const labels = document.querySelectorAll("svg text");
   console.log("labels:", labels.length);
 }
+
+const tooltip = document.getElementById("tooltip");
 
 Promise.all([
   fetch("./tree.svg").then(r => r.text()),
   fetch("./metadata.json").then(r => r.json())
 ]).then(([svg, metadata]) => {
   document.getElementById("tree-container").innerHTML = svg;
-  attachTooltips(metadata);
   console.log("SVG + metadata loaded");
+
+  // grab iTOL leaf labels (<text> elements)
+  const labels = document.querySelectorAll("svg text");
+  console.log("labels:", labels.length);
+
+  labels.forEach(label => {
+    const id = label.textContent.trim(); // remove whitespace
+    
+    if (!metadata[id]) return; // skip label if not in metadata
+
+    label.classList.add("tree-label");
+
+    label.addEventListener("mouseover", e => {
+      tooltip.innerHTML = `
+        <b>${id}</b><br>
+        ${metadata[id].species}<br>
+        ${metadata[id].taxonomy}<br>
+        ${metadata[id].environment}
+      `;
+      tooltip.style.display = "block";
+    });
+
+    label.addEventListener("mousemove", e => {
+      tooltip.style.left = e.pageX + 10 + "px";
+      tooltip.style.top = e.pageY + 10 + "px";
+    });
+
+    label.addEventListener("mouseout", () => {
+      tooltip.style.display = "none";
+    });
+  });
 }).catch(console.error);
 
 
-
-
-const tooltip = document.getElementById("tooltip");
-
-
-// iTOL leaf labels are <text> elements
-const labels = document.querySelectorAll("svg tspan, svg text");
-console.log("Number of SVG text labels found:", labels.length);
-
-// labels.forEach(label => {
-//   const id = label.textContent.trim();
-
-//   if (!metadata[id]) return;
-
-//   label.classList.add("tree-label");
-
-//   label.addEventListener("mouseover", e => {
-//     tooltip.innerHTML = `
-//       <b>${id}</b><br>
-//       ${metadata[id].species}<br>
-//       ${metadata[id].taxonomy}<br>
-//       ${metadata[id].environment}
-//     `;
-//     tooltip.style.display = "block";
-//   });
-
-//   label.addEventListener("mousemove", e => {
-//     tooltip.style.left = e.pageX + 10 + "px";
-//     tooltip.style.top = e.pageY + 10 + "px";
-//   });
-
-//   label.addEventListener("mouseout", () => {
-//     tooltip.style.display = "none";
-//   });
-// });
